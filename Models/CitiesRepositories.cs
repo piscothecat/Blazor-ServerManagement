76eccf252797c452b2c2e85adedb121062471c1e
piscothecat <152401425+piscothecat@users.noreply.github.com>
2024-06-09 21:28:33 +0200

    Add files via upload

---
+++ Models/CitiesRepositories.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagement.Models
+{
+    public static class CitiesRepositories
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => cities;
+
+    }
+}
